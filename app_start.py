8aa8b989be967cfc62ae597f9716007cd722ce46
tkgka <ksh17171@gmail.com>
2021-11-10 21:55:50 +0900

    ./

--- app_start.py
+++ app_start.py
@@ -1,4 +1,17 @@
-from prototype import app
-
+from flask import Flask, request
+import flask
+import os
+app = Flask(__name__)
+ 
+@app.route("/")
+def hello():
+    url = request.args.get('url')
+    if url != None:
+        stream = os.popen('curl --head "'+url+'" | grep Location')
+        output= flask.Response(stream.read())
+        output.headers["Access-Control-Allow-Origin"] = "*"
+        return output
+    else:
+        return "None"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8080", debug=False)
