6f00a1f02a52a5ad6519cffce34d1d6af6226930
tkgka <52348220+tkgka@users.noreply.github.com>
2021-11-10 21:16:08 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import flask
+import os
+app = Flask(__name__)
+ 
+@app.route("/")
+def hello():
+    url = request.args.get('url')
+    if url != None:
+        stream = os.popen('curl --head "'+url+'" | grep Location')
+        output= flask.Response(stream.read())
+        output.headers["Access-Control-Allow-Origin"] = "*"
+        return output
+    else:
+        return "None"
+ 
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port="8080", debug=False)
