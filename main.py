caf0bef998974126679634deb4242b5f944d4a68
tkgka <ksh17171@gmail.com>
2021-11-15 20:56:38 +0900

    ./

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect, url_for
 import flask
 import os
+import imgkit
+import json
+options = {
+    'format': 'png',
+    'height': 500, 
+    'encoding': "UTF-8",
+}
+
 app = Flask(__name__)
  
 @app.route("/")
-def hello():
+def fullurl():
     url = request.args.get('url')
     if url != None:
         stream = os.popen('curl --head "'+url+'" | grep Location')
         output= flask.Response(stream.read())
-        output.headers["Access-Control-Allow-Origin"] = "*"
+        output.headers["Access-Control-Allow-Origin"] = "*"        
         return output
+    
     else:
         return "None"
+@app.route("/img/")
+def img():    
+    url = request.args.get('url')
+    img = imgkit.from_url(url, False, options=options)
+    return flask.Response(img, mimetype="image/png")
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
